38a9dbc8ef39a4f954956bad48eecd47747cccf4
gunny <corqkrtk3@naver.com>
2020-02-11 22:34:04 +0900

    Refactor @FunctionalInterface Annotation to PreparedStatementSetter and RowMapper

--- src/main/java/user/dao/PreparedStatementSetter.java
+++ src/main/java/user/dao/PreparedStatementSetter.java
@@ -3,6 +3,7 @@ package user.dao;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface PreparedStatementSetter {
     void values(PreparedStatement pstmt) throws SQLException;
 }
--- src/main/java/user/dao/RowMapper.java
+++ src/main/java/user/dao/RowMapper.java
@@ -3,6 +3,7 @@ package user.dao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface RowMapper<T> {
     T mapRow(ResultSet rs) throws SQLException;
 }
